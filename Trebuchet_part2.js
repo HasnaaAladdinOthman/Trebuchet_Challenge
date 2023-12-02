8c8ddcad0c7f72396d789d0cabfa2a80029b41be
Hasnaa Aladdin <36827774+HasnaaAladdinOthman@users.noreply.github.com>
2023-12-02 19:12:53 +0200

    Update Trebuchet_part2.js

--- Trebuchet_part2.js
+++ Trebuchet_part2.js
@@ -1,17 +1,16 @@
 import fs from "fs";
 var answer=0;
-var lineNumber=0;
 const digitsMapping = {
-    'zero': '0',
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
+    'zero': 'z0o',
+    'one': 'o1e',
+    'two': 't2o',
+    'three': 't3e',
+    'four': 'f4r',
+    'five': 'f5e',
+    'six': 's6x',
+    'seven': 's7n',
+    'eight': 'e8t',
+    'nine': 'n9e'
   };
 
 //Create a regular expression pattern
@@ -21,30 +20,20 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
     if (err) throw err;
     const lines= data.trim().split(/\r?\n/);
     lines.forEach(line => {
-        lineNumber++;
+        //replacing the spelledout letters with digits using the digitsMapping object
+        line = line.replace(pattern, (match) => digitsMapping[match]);
+        line = line.replace(pattern, (match) => digitsMapping[match]);
         var digits=[];
-        //traverse through the line to look for any numbers
-        for (let i = 0; i < line.length; i++) {
-            let currentNumber = '';
+        //traverse through the line to find digits
+        for(let i=0;i<line.length;i++){
             if(!isNaN(line[i])){
                 digits.push(line[i]);
-                continue;
             }
-            
-            for (let j = i; j < line.length; j++) {
-              currentNumber += line[j];
-              
-              if (digitsMapping.hasOwnProperty(currentNumber)) {
-                digits.push(digitsMapping[currentNumber]);
-              }
-            }
-          }
-
+        }
         var lineDigits= digits.length > 0 ? digits[0] + digits[digits.length - 1] : '';
         if (lineDigits) {
             answer += Number(lineDigits);
-        }
-        
+          }
     });
     console.log("answer is "+ answer);
 });
